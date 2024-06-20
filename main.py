ae534b6ca4031a3e7e6807b00cb56ef76a13d8f8
Casey Johnson <sharus.programmer@gmail.com>
2024-06-20 12:07:48 +0700

    First commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from typing import Annotated
+
+from fastapi import FastAPI, Query
+
+app = FastAPI()
+
+
+@app.get("/items/")
+async def read_items(q: Annotated[str, Query(min_length=3)]):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+@app.post("/itemsd/")
+async def read_itemss(q: Annotated[str, Query(min_length=3)]):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
